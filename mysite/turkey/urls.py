87e8d3e433691d3cc68cbf8132b369882082f8ae
Arlene122 <36873708+Arlene122@users.noreply.github.com>
2019-06-06 13:02:57 +1000

    startclass2

--- mysite/turkey/urls.py
+++ mysite/turkey/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path(r'example_get/<str:var_a>/<int:var_b>',  views.example_get), #this is the name of a funciton#
-	path(r'example_post/', views.example_post),
+	#path(r'example_get/<str:var_a>/<int:var_b>',  views.example_get), #this is the name of a funciton#
+	#path(r'example_post/', views.example_post),
 ]
