8ec34d146148005930e465c9b19f4233c3b7d438
Arlene122 <36873708+Arlene122@users.noreply.github.com>
2019-06-13 12:33:26 +1000

    class 2 fib

--- mysite/example/urls.py
+++ mysite/example/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
 	path(r'example_get/<str:var_a>/<int:var_b>',  views.example_get), #this is the name of a funciton#
 	path(r'example_post/', views.example_post),
+	path(r'fib/', views.fib), #this is the address to access the definition
 ]
--- mysite/example/views.py
+++ mysite/example/views.py
@@ -3,10 +3,11 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 import json
 from .models import *
+import os, sys
 
 # Create your views here.
 
-def example_get(request, var_a, var_b):#these variables must be the same name as in the function in the url file#
+def example_get(request, var_a, var_b):#these variables must be the same name as in the function in the url file.  This is running a get request
 	try:#Try - this prevents Django from breaking, and gives an object about the error and tells you about the error#
 		returnob = {
 		"data": "bubblegum %s < pizza %s" %(var_a, var_b),
@@ -19,13 +20,19 @@ def example_get(request, var_a, var_b):#these variables must be the same name as
 		errorType = str(exc_type)
 		return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
 
-@csrf_exempt
-def example_post(request):
+@csrf_exempt #this is a decorator -> These are put before a function
+def example_post(request): #example_post -> This allows you to send data with the request
 	log = []
 	if request.method == "POST":
 		try:
 			data = request.POST["data"]
-			return JsonResponse({"log":log})
+			jsob = json.loads(data) #changes from this line are from Lecture 2 recording
+
+			index = 0
+			for i in jsob["demo"]:
+				index += 1
+
+			return JsonResponse({"count":index})
 		except Exception as e:
 			exc_type, exc_obj, exc_tb = sys.exc_info()
 			other = sys.exc_info()[0].__name__
@@ -33,4 +40,44 @@ def example_post(request):
 			errorType = str(exc_type)
 			return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
 	else:
-		return HttpResponse("ONLY POST REQUESTS")
+		return HttpResponse("ONLY POST REQUESTS") #this is returning HTML
+
+
+
+
+
+@csrf_exempt #this is a decorator -> These are put before a function
+def fib(request): #example_post -> This allows you to send data with the request
+	jsob = {"StartNumber": 0, "Length": 10}
+	log = [] #helps to print errors if there are problems
+	if request.method == "POST":
+		try:
+			data = request.POST["data"]
+			received = json.loads(data)
+			jsob.update(received) #changes from this line are from Lecture 2 recording
+
+			#######################################
+			#EVERYTHING ABOVE THIS LINE IS REQURED#
+			#######################################
+
+			startnumber = int(jsob["StartNumber"]) #int makes sure that even if someone gives us a string it is now an integer
+			length= int(jsob["Length"])
+			loop = range(length) #this is making an array of numbers
+
+			numarray = []
+			fibno = startnumber #this is the current number
+			addno = 1
+			for l in loop:
+				numarray.append(fibno)
+				fibno = fibno + addno
+				addno = fibno - addno
+
+			return JsonResponse({"fib":numarray})
+		except Exception as e:
+			exc_type, exc_obj, exc_tb = sys.exc_info()
+			other = sys.exc_info()[0].__name__
+			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+			errorType = str(exc_type)
+			return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
+	else:
+		return JsonResponse(jsob) #this is returning HTML
