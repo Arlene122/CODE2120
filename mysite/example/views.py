95c6d8481c28f3b35f0327ae30bd828422db73cc
Arlene122 <36873708+Arlene122@users.noreply.github.com>
2019-08-15 15:24:34 +1000

    weather try 1

--- mysite/example/views.py
+++ mysite/example/views.py
@@ -101,23 +101,69 @@ def practice(request): #example_post -> This allows you to send data with the re
 			#EVERYTHING ABOVE THIS LINE IS REQURED#
 			#######################################
 
-			CounryName = int(jsob["CountryName"]) #int makes sure that even if someone gives us a string it is now an integer
-			Weather= int(jsob["Weather"])
+			
 
-			numarray = []
-			fibno = startnumber #this is the current number
-			addno = 1
-			for l in loop:
-				numarray.append(fibno)
-				fibno = fibno + addno
-				addno = fibno - addno
 
-			return JsonResponse({"practice":numarray})
-		except Exception as e:
-			exc_type, exc_obj, exc_tb = sys.exc_info()
-			other = sys.exc_info()[0].__name__
-			fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-			errorType = str(exc_type)
-			return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
-	else:
-		return JsonResponse(jsob) #this is returning HTML
+
+
+			# base_url variable to store url 
+			base_url = "http://api.openweathermap.org/data/2.5/weather?"
+			  
+			# Give city name 
+			city_name = input("Enter city name : ") 
+			  
+			# complete_url variable to store 
+			# complete url address 
+			complete_url = base_url + "appid=" + api_key + "&q=" + city_name 
+			  
+			# get method of requests module 
+			# return response object 
+			response = requests.get(complete_url) 
+			  
+			# json method of response object  
+			# convert json format data into 
+			# python format data 
+			x = response.json() 
+			  
+			# Now x contains list of nested dictionaries 
+			# Check the value of "cod" key is equal to 
+			# "404", means city is found otherwise, 
+			# city is not found 
+			if x["cod"] != "404": 
+			  
+			    # store the value of "main" 
+			    # key in variable y 
+			    y = x["main"] 
+			  
+			    # store the value corresponding 
+			    # to the "temp" key of y 
+			    current_temperature = y["temp"] 
+			  
+			  
+			    # store the value of "weather" 
+			    # key in variable z 
+			    z = x["weather"] 
+			  
+			    # store the value corresponding  
+			    # to the "description" key at  
+			    # the 0th index of z 
+			    weather_description = z[0]["description"] 
+			  
+			    # print following values 
+			    return JsonResponse({"practice" +
+			                    str(current_temperature) + 
+			          "\n atmospheric pressure (in hPa unit) = " +
+
+			          "\n description = " +
+			                    str(weather_description)}) 
+			  
+			except Exception as e:
+				exc_type, exc_obj, exc_tb = sys.exc_info()
+				other = sys.exc_info()[0].__name__
+				fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+				errorType = str(exc_type)
+				return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
+		else:
+			return JsonResponse(jsob) #this is returning HTML
+
+
